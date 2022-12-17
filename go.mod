ea5c1e8cdb2fc1b7e31dc5cd3a7aeeb887bdaf52
daikidev111 <dkub0001@student.monash.edu>
2022-12-18 03:42:48 +0900

    a2

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/daikidev111/finishgomock v0.0.0-20221217092441-727005745ad8 // indirect
+	github.com/sivchari/unusedtypeparam v1.0.0 // indirect
 	golang.org/x/tools v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+}
+
+func okFirst(t *testing.T) {
+	mock := gomock.NewController(t)
+	mock.Finish() // want "detected an unnecessary call to Finish on gomock.Controllers"
+}
+
+func okSecond(t *testing.T) {
+	mock := gomock.NewController(t)
+	defer mock.Finish() // want "detected an unnecessary call to Finish on gomock.Controllers"
+}
+
+func failFirst(t *testing.T) {
+	gomock.NewController(t)
+}
+
 }
